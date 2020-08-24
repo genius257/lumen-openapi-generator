f33a60f67677e1ed8a560955b01c697f163e1349
Anders Pedersen <hot415@hotmail.com>
2020-08-24 23:58:19 +0200

    Update GeneratorTest.php

--- tests/GeneratorTest.php
+++ tests/GeneratorTest.php
@@ -14,9 +14,15 @@ class GeneratorTest extends TestCase {
              * example router description here.
              * @tag tag01
              * @tag tag02
+             * @response 200 {@class \Illuminate\Database\Eloquent\Model} this is a test
+             * @response 404 {
+             *  "message": "No query results for model"
+             * } a 404 case
+             * @response 200 invalid response tag. Will be omitted from result.
+             * @class 123
              */
             function () {
-            //
+                //
             }
         );
         $openAPI = Generator::Generate($app->router);
